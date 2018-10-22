ee583573254416bba06292a1a2407465f6f40f65
student <student@PC-10>
2018-10-22 18:37:41 +0200

    novo

--- WeatherStationObserving/src/App.java
+++ WeatherStationObserving/src/App.java
@@ -3,7 +3,17 @@ public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		WeatherStationObs wso1 = new WeatherStationObs();
+		Mobitel mob1= new Mobitel(wso1);
+		
+		wso1.add(mob1);
+		
+		wso1.setAll(21, 15, 12);
+						
+		System.out.println("************************");
+		wso1.setAll(12, 9, 2);
+		
 	}
 
 }
--- WeatherStationObserving/src/Mobitel.java
+++ WeatherStationObserving/src/Mobitel.java
@@ -2,6 +2,10 @@
 public class Mobitel implements Observer, Display {
 	
 	WeatherStationObs wthStation;
+	private float temp;
+	private float hum;
+	private float press;
+	
 	
 	public Mobitel(WeatherStationObs wthStation) {
 		
@@ -14,13 +18,19 @@ public class Mobitel implements Observer, Display {
 	@Override
 	public void display() {
 		// TODO Auto-generated method stub
-
+		System.out.println("Temp: "+temp);
+		System.out.println("Hummidity: "+ hum);
+		System.out.println("Preassure: "+ press);
+		
 	}
 
 	@Override
 	public void update() {
 		// TODO Auto-generated method stub
-
+		this.temp=wthStation.getTemp();
+		this.hum=wthStation.getHum();
+		this.press=wthStation.getPress();
+		display();
 	}
 
 }
